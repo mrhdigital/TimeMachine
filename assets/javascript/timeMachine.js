f16f952e06f757fccaddd8e73f54b2583d33e632
unknown <mrhdigital@yahoo.com>
2019-11-09 16:44:30 -0600

    rotate minute hand every minute

--- assets/javascript/timeMachine.js
+++ assets/javascript/timeMachine.js
@@ -1,11 +1,18 @@
 //alert("Hello");
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+
+const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     //console.log("Hi");
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     console.log(seconds);
 }
