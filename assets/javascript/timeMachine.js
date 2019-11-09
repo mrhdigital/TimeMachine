4f52f92851d24e4d7cf551b3498cd8583479983b
unknown <mrhdigital@yahoo.com>
2019-11-09 16:35:41 -0600

    rotate second hand every second

--- assets/javascript/timeMachine.js
+++ assets/javascript/timeMachine.js
@@ -1,7 +1,13 @@
 //alert("Hello");
-
+const secondHand = document.querySelector('.second-hand');
 function setDate() {
-    console.log("Hi");
+    //console.log("Hi");
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    console.log(seconds);
 }
  setInterval(setDate,1000);
 //setDate();
