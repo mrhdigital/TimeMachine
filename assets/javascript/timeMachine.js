1569cd5a8c38e0b7c48da48378f767018ace27a6
unknown <mrhdigital@yahoo.com>
2019-11-09 21:21:38 -0600

    changed sec hand and outside section

--- assets/javascript/timeMachine.js
+++ assets/javascript/timeMachine.js
@@ -1,21 +1,21 @@
 //alert("Hello");
 const secondHand = document.querySelector('.second-hand');
-const minuteHand = document.querySelector('.min-hand');
+const minuteHand = document.querySelector('.minutehand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     //console.log("Hi");
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 60) * 360) +90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360)+260;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hour = now.getHours();
-    const hoursDegrees = ((hour / 12) * 360) + 90;
+    const hoursDegrees = ((hour / 12) * 360) -90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
 
